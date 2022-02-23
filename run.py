987249222437e8c38bdf951adbde227bdb907a21
Sabinastorm <sabinasaila@icloud.com>
2022-02-23 19:51:18 +0000

    Fix bugs

--- run.py
+++ run.py
@@ -19,41 +19,41 @@ def play(word):
     while not guessed and tries > 0:
         guess = input("Guess a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
-           if guess in guessed_letters:
-               print("You already guessed the letter", guess)
-           elif guess not in word:
-               print(guess, "is not in the word.")
-               tries -= 1
-               guessed_letters.append(guess)
-           else:
-               print("Well done,", guess, "is in the word!")
-               guessed_letters.append(guess)
-               word_as_list = list(word_completion)
-               indices = [i for i, letter in enumerate(word) if letter == guess]
-               for index in indices:
-                   word_as_list[index] = guess
-                   word_completion = "".join(word_as_list)
-                   if "_" not in word_completion:
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Well done,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                    word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
                        guessed = True    
         elif len(guess) == len(word) and guess.isalpha():
-               if guess in guessed_words:
-                   print("You already guessed the word", guess)
-               elif guess != word:
-                   print(guess, "is not the word.")
-                   tries -= 1
-                   guessed_words.append(guess)
-               else:
-                   guessed = True
-                   word_completion = word        
+            if guess in guessed_words:
+               print("You already guessed the word", guess)
+            elif guess != word:
+               print(guess, "is not the word.")
+               tries -= 1
+               guessed_words.append(guess)
+            else:
+               guessed = True
+               word_completion = word        
         else:
             print("Not a valid guess.")
-            print(display_hangman(tries))
-            print(word_completion)
-            print("\n")    
-        if guessed:
-            print("Congratulations, you guessed the word! You win!")
-        else:
-            print("Sorry, you didn't guess the word this time. The word was " + word + ". Better luck next time!")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")    
+    if guessed:
+        print("Congratulations, you guessed the word! You win!")
+    else:
+        print("Sorry, you didn't guess the word this time. The word was " + word + ". Better luck next time!")
 
 def display_hangman(tries):
     stages = [  """
@@ -63,6 +63,7 @@ def display_hangman(tries):
                     |            \ / 
                     |             | 
                     |            / \  
+                    |
 
                 """,
                 """ 
@@ -72,6 +73,7 @@ def display_hangman(tries):
                     |            \ / 
                     |             | 
                     |            / 
+                    |
 
                 """,
                 """      
@@ -82,6 +84,7 @@ def display_hangman(tries):
                     |            \ / 
                     |             | 
                     |            
+                    |
 
                 """,
                 """
@@ -91,6 +94,7 @@ def display_hangman(tries):
                     |            \ / 
                     |             
                     |             
+                    |
 
                 """,
                 """
@@ -100,6 +104,7 @@ def display_hangman(tries):
                     |            \ 
                     |             
                     |              
+                    |
 
                 """,
                 """
@@ -109,6 +114,7 @@ def display_hangman(tries):
                     |            
                     |              
                     |         
+                    |
 
                 """,
                 """
@@ -118,7 +124,8 @@ def display_hangman(tries):
                     |            
                     |             
                     |     
-               
+                    |
+                    
                 """                           
     ]
     return stages[tries]
@@ -126,12 +133,12 @@ def display_hangman(tries):
 
 
 
-        def main():
-             word = get_word()
-             play(word)
-             while input("Play again? (Y/N) ").upper() == "Y":
-                 word = get_word()
-                 play(word)  
+def main():
+    word = get_word()
+    play(word)
+    while input("Play again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)  
 
 if __name__== "__main__":
     main()
